379d75deb799ec5c9cda8e8097ca3a0f4636c1e2
Faisal Al-Hawajreh <faisal.alhawajreh@gmail.com>
2022-01-29 23:59:51 +0200

    regex test

--- regex/regex.test.js
+++ regex/regex.test.js
@@ -6,8 +6,9 @@
 characters (no numbers and symbols) and it should end with capital A else return false */
 
 function capitalA(s){
+    let validation = /[A-Z]\w+ \w+ (A)/
     // Add your logic.
-    return;
+    return validation.test(s);
 }
 
 
@@ -15,8 +16,9 @@ function capitalA(s){
 which end with io (example@example.io) */
 
 function ioEmail(email){
-    // Add your logic.
-    return;
+    let validation = /^\w+(@)\w+.(io)$/
+    // Add your logic. 
+    return validation.test(email);
 }
 
 /* You have a text that contain image names with their extention you need to write a function to 
